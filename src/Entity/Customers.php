799fe33e44a56eb46ac9db0de8bc91da037ecb74
YoanFay <yoanfayolle.yf@gmail.com>
2023-11-02 14:56:36 +0100

    asset

--- src/Entity/Customers.php
+++ src/Entity/Customers.php
@@ -9,8 +9,11 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
+ * @UniqueEntity(fields={"email"}, message="L'email {{ value }} est déjà utilisé")
  * @ORM\Entity(repositoryClass=CustomersRepository::class)
  */
 class Customers implements UserInterface, PasswordAuthenticatedUserInterface
@@ -26,12 +29,21 @@ class Customers implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @ORM\Column(type="string", length=180, unique=true)
      * @Groups("getCustomers", "getUsers")
+     * @Assert\NotBlank(message = "L'email est obligatoire")
+     * @Assert\Email(message = "L'e-mail '{{ value }}' n'est pas un e-mail valide.")
+     * @Assert\Length(
+     *      min = 6,
+     *      max = 180,
+     *      minMessage = "Votre e-mail doit contenir au moins {{ limit }} caractères.",
+     *      maxMessage = "Votre e-mail ne peut pas comporter plus de {{ limit }} caractères."
+     * )
      */
     private $email;
 
     /**
      * @ORM\Column(type="json")
      * @Groups("getCustomers", "getUsers")
+     * @Assert\NotBlank(message = "Les rôles sont obligatoires")
      */
     private $roles = [];
 
@@ -39,18 +51,33 @@ class Customers implements UserInterface, PasswordAuthenticatedUserInterface
      * @var string The hashed password
      * @ORM\Column(type="string")
      * @Groups("getCustomers", "getUsers")
+     * @Assert\NotBlank(message = "Le mot de passe est obligatoires")
      */
     private $password;
 
     /**
      * @ORM\Column(type="string", length=50)
      * @Groups("getCustomers", "getUsers")
+     * @Assert\NotBlank(message = "Le prénom est obligatoires")
+     * @Assert\Length(
+     *      min = 3,
+     *      max = 50,
+     *      minMessage = "Votre prénom doit comporter au moins {{ limit }} caractères.",
+     *      maxMessage = "Votre prénom ne peut pas contenir plus de {{ limit }} caractères."
+     * )
      */
     private $firstname;
 
     /**
      * @ORM\Column(type="string", length=50)
      * @Groups("getCustomers", "getUsers")
+     * @Assert\NotBlank(message = "Le nom est obligatoires")
+     * @Assert\Length(
+     *      min = 3,
+     *      max = 50,
+     *      minMessage = "Votre nom de famille doit comporter au moins {{ limit }} caractères.",
+     *      maxMessage = "Votre nom de famille ne peut pas comporter plus de {{ limit }} caractères."
+     * )
      */
     private $lastname;
 
@@ -60,27 +87,36 @@ class Customers implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $users;
 
+
     public function __construct()
     {
+
         $this->users = new ArrayCollection();
     }
 
+
     public function getId(): ?int
     {
+
         return $this->id;
     }
 
+
     public function getEmail(): ?string
     {
+
         return $this->email;
     }
 
+
     public function setEmail(string $email): self
     {
+
         $this->email = $email;
 
         return $this;
     }
+
 
     /**
      * A visual identifier that represents this user.
@@ -89,22 +125,27 @@ class Customers implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUserIdentifier(): string
     {
-        return (string) $this->email;
+
+        return (string)$this->email;
     }
+
 
     /**
      * @deprecated since Symfony 5.3, use getUserIdentifier instead
      */
     public function getUsername(): string
     {
-        return (string) $this->email;
+
+        return (string)$this->email;
     }
+
 
     /**
      * @see UserInterface
      */
     public function getRoles(): array
     {
+
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
@@ -112,27 +153,34 @@ class Customers implements UserInterface, PasswordAuthenticatedUserInterface
         return array_unique($roles);
     }
 
+
     public function setRoles(array $roles): self
     {
+
         $this->roles = $roles;
 
         return $this;
     }
+
 
     /**
      * @see PasswordAuthenticatedUserInterface
      */
     public function getPassword(): string
     {
+
         return $this->password;
     }
 
+
     public function setPassword(string $password): self
     {
+
         $this->password = $password;
 
         return $this;
     }
+
 
     /**
      * Returning a salt is only needed, if you are not using a modern
@@ -142,52 +190,67 @@ class Customers implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getSalt(): ?string
     {
+
         return null;
     }
+
 
     /**
      * @see UserInterface
      */
     public function eraseCredentials()
     {
+
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
 
+
     public function getFirstname(): ?string
     {
+
         return $this->firstname;
     }
 
+
     public function setFirstname(string $firstname): self
     {
+
         $this->firstname = $firstname;
 
         return $this;
     }
 
+
     public function getLastname(): ?string
     {
+
         return $this->lastname;
     }
 
+
     public function setLastname(string $lastname): self
     {
+
         $this->lastname = $lastname;
 
         return $this;
     }
+
 
     /**
      * @return Collection<int, Users>
      */
     public function getUsers(): Collection
     {
+
         return $this->users;
     }
 
+
     public function addUser(Users $user): self
     {
+
         if (!$this->users->contains($user)) {
             $this->users[] = $user;
             $user->setCustomer($this);
@@ -196,8 +259,10 @@ class Customers implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+
     public function removeUser(Users $user): self
     {
+
         if ($this->users->removeElement($user)) {
             // set the owning side to null (unless already changed)
             if ($user->getCustomer() === $this) {
--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\UsersRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UsersRepository::class)
@@ -22,36 +23,73 @@ class Users
     /**
      * @ORM\Column(type="string", length=50)
      * @Groups("getUsers", "getCustomers")
+     * @Assert\NotBlank(message = "Le prénom est obligatoires")
+     * @Assert\Length(
+     *      min = 3,
+     *      max = 50,
+     *      minMessage = "Votre prénom doit comporter au moins {{ limit }} caractères.",
+     *      maxMessage = "Votre prénom ne peut pas contenir plus de {{ limit }} caractères."
+     * )
      */
     private $firstname;
 
     /**
      * @ORM\Column(type="string", length=50)
      * @Groups("getUsers", "getCustomers")
+     * @Assert\NotBlank(message = "Le nom est obligatoires")
+     * @Assert\Length(
+     *      min = 3,
+     *      max = 50,
+     *      minMessage = "Votre nom de famille doit comporter au moins {{ limit }} caractères.",
+     *      maxMessage = "Votre nom de famille ne peut pas comporter plus de {{ limit }} caractères."
+     * )
      */
     private $lastname;
 
     /**
      * @ORM\Column(type="string", length=100)
      * @Groups("getUsers", "getCustomers")
+     * @Assert\NotBlank(message = "L'email est obligatoire")
+     * @Assert\Email(message = "L'e-mail '{{ value }}' n'est pas un e-mail valide.")
+     * @Assert\Length(
+     *      min = 6,
+     *      max = 100,
+     *      minMessage = "Votre e-mail doit contenir au moins {{ limit }} caractères.",
+     *      maxMessage = "Votre e-mail ne peut pas comporter plus de {{ limit }} caractères."
+     * )
      */
     private $email;
 
     /**
      * @ORM\Column(type="string", length=100)
      * @Groups("getUsers", "getCustomers")
+     * @Assert\NotBlank(message = "L'adresse est obligatoire")
+     * @Assert\Length(
+     *      min = 6,
+     *      max = 100,
+     *      minMessage = "L'adresse doit contenir au moins {{ limit }} caractères.",
+     *      maxMessage = "L'adresse ne peut pas comporter plus de {{ limit }} caractères."
+     * )
      */
     private $address;
 
     /**
      * @ORM\Column(type="string", length=50)
      * @Groups("getUsers", "getCustomers")
+     * @Assert\NotBlank(message = "La ville est obligatoire")
+     * @Assert\Length(
+     *      min = 6,
+     *      max = 50,
+     *      minMessage = "La ville doit contenir au moins {{ limit }} caractères.",
+     *      maxMessage = "La ville ne peut pas comporter plus de {{ limit }} caractères."
+     * )
      */
     private $city;
 
     /**
      * @ORM\Column(type="integer")
      * @Groups("getUsers", "getCustomers")
+     * @Assert\NotBlank(message = "Le code postal est obligatoire")
      */
     private $zipcode;
 
