d8aad10144d4c751bf3f53268bb085a793b337cb
YoanFay <yoanfayolle.yf@gmail.com>
2023-12-01 11:42:11 +0100

    Medium

--- src/DataFixtures/CustomersFixtures.php
+++ src/DataFixtures/CustomersFixtures.php
@@ -12,74 +12,28 @@ class CustomersFixtures extends Fixture
 
     private $userPasswordHasher;
 
-    public function __construct(UserPasswordHasherInterface $userPasswordHasher){
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+
         $this->userPasswordHasher = $userPasswordHasher;
     }
+
 
     public function load(ObjectManager $manager): void
     {
 
         $customers = [
-            [
-                'firstname' => 'John',
-                'lastname'  => 'Doe',
-                'email'     => 'johndoe@example.com',
-                'password'  => 'motdepasse123',
-            ],
-            [
-                'firstname' => 'Alice',
-                'lastname'  => 'Smith',
-                'email'     => 'alice@example.com',
-                'password'  => 'mdpAlice123'
-            ],
-            [
-                'firstname' => 'Bob',
-                'lastname'  => 'Johnson',
-                'email'     => 'bob@example.com',
-                'password'  => 'mdpBob456'
-            ],
-            [
-                'firstname' => 'Claire',
-                'lastname'  => 'Williams',
-                'email'     => 'claire@example.com',
-                'password'  => 'mdpClaire789'
-            ],
-            [
-                'firstname' => 'David',
-                'lastname'  => 'Anderson',
-                'email'     => 'david@example.com',
-                'password'  => 'mdpDavid123'
-            ],
-            [
-                'firstname' => 'Eva',
-                'lastname'  => 'Brown',
-                'email'     => 'eva@example.com',
-                'password'  => 'mdpEva456'
-            ],
-            [
-                'firstname' => 'Frank',
-                'lastname'  => 'Davis',
-                'email'     => 'frank@example.com',
-                'password'  => 'mdpFrank789'
-            ],
-            [
-                'firstname' => 'Grace',
-                'lastname'  => 'Wilson',
-                'email'     => 'grace@example.com',
-                'password'  => 'mdpGrace123'
-            ],
-            [
-                'firstname' => 'Henry',
-                'lastname'  => 'Martinez',
-                'email'     => 'henry@example.com',
-                'password'  => 'mdpHenry456'
-            ],
-            [
-                'firstname' => 'Ivy',
-                'lastname'  => 'Garcia',
-                'email'     => 'ivy@example.com',
-                'password'  => 'mdpIvy789'
-            ]
+            ['firstname' => 'John', 'lastname' => 'Doe', 'email' => 'johndoe@example.com', 'password' => 'motdepasse123'],
+            ['firstname' => 'Alice', 'lastname' => 'Smith', 'email' => 'alice@example.com', 'password' => 'mdpAlice123'],
+            ['firstname' => 'Bob', 'lastname' => 'Johnson', 'email' => 'bob@example.com', 'password' => 'mdpBob456'],
+            ['firstname' => 'Claire', 'lastname' => 'Williams', 'email' => 'claire@example.com', 'password' => 'mdpClaire789'],
+            ['firstname' => 'David', 'lastname' => 'Anderson', 'email' => 'david@example.com', 'password' => 'mdpDavid123'],
+            ['firstname' => 'Eva', 'lastname' => 'Brown', 'email' => 'eva@example.com', 'password' => 'mdpEva456'],
+            ['firstname' => 'Frank', 'lastname' => 'Davis', 'email' => 'frank@example.com', 'password' => 'mdpFrank789'],
+            ['firstname' => 'Grace', 'lastname' => 'Wilson', 'email' => 'grace@example.com', 'password' => 'mdpGrace123'],
+            ['firstname' => 'Henry', 'lastname' => 'Martinez', 'email' => 'henry@example.com', 'password' => 'mdpHenry456'],
+            ['firstname' => 'Ivy', 'lastname' => 'Garcia', 'email' => 'ivy@example.com', 'password' => 'mdpIvy789']
         ];
 
         foreach ($customers as $customer) {
--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -11,6 +11,23 @@ class ExceptionSubscriber implements EventSubscriberInterface
 {
     public function onKernelException(ExceptionEvent $event): void
     {
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof HttpException) {
+            $data = [
+                'status' => $exception->getStatusCode(),
+                'message' => $exception->getMessage()
+            ];
+
+            $event->setResponse(new JsonResponse($data));
+        } else {
+            $data = [
+                'status' => 500,
+                'message' => $exception->getMessage()
+            ];
+
+            $event->setResponse(new JsonResponse($data));
+        }
     }
 
     public static function getSubscribedEvents(): array
